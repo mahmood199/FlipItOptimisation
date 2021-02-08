85cb32c45f4f23a78f0f0f4c9276c38b0927ab97
NoBody <gaudentas199@gmail.com>
2021-02-08 20:53:40 +0530

    Removing the Status bar from SixCrossSix activity

--- app/src/main/java/com/example/flipit/SixCrossSix.java
+++ app/src/main/java/com/example/flipit/SixCrossSix.java
@@ -45,9 +45,12 @@ public class SixCrossSix extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_six_cross_six);
 
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         mContext=this;
         text_top=(TextView) findViewById(R.id.six_cross_six_top_text);
         text_below=(TextView) findViewById(R.id.six_cross_six_bottom_text);
